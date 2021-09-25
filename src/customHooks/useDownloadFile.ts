059d220fbf1d872154e238b1b375f2a5e64cc668
Anubhav Goel <replyag96@gmail.com>
2021-09-25 21:48:37 +0530

    Added: useDownloadFile hook

---
+++ src/customHooks/useDownloadFile.ts
@@ -0,0 +1,45 @@
+import { AxiosResponse } from "axios";
+import { useRef, useState } from "react";
+
+interface DownloadFileProps {
+  readonly asyncFunction: () => Promise<AxiosResponse>;
+  readonly preDownloading: () => void;
+  readonly postDownloading: () => void;
+  readonly onError: () => void;
+  readonly getFileName: () => string;
+}
+
+interface DownloadedFileInfo {
+  readonly downloadFile: () => Promise<void>;
+  readonly fileRef: React.MutableRefObject<HTMLAnchorElement | null>;
+  readonly fileName: string | undefined;
+  readonly fileUrl: string | undefined;
+}
+
+export const useDownloadFile = ({
+  asyncFunction,
+  preDownloading,
+  postDownloading,
+  onError,
+  getFileName,
+}: DownloadFileProps): DownloadedFileInfo => {
+  const fileRef = useRef<HTMLAnchorElement | null>(null);
+  const [fileUrl, setFileUrl] = useState<string>();
+  const [fileName, setFileName] = useState<string>();
+
+  const downloadFile = async () => {
+    try {
+      preDownloading();
+      const { data } = await asyncFunction();
+      const url = URL.createObjectURL(new Blob([data]));
+      setFileUrl(url);
+      setFileName(getFileName());
+      fileRef.current?.click();
+      postDownloading();
+    } catch (error) {
+      onError();
+    }
+  };
+
+  return { downloadFile, fileRef, fileUrl, fileName };
+};
